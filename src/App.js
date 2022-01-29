361d60ef4c4e29d455a2e49bfaf1792efe4646db
miguepoloc <miguepoloc@gmail.com>
2022-01-28 20:21:13 -0500

    Instalación de router y inicio del formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 /* eslint-disable react/style-prop-object */
 // import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
+import Formulario from './components/Formulario';
 
 function App() {
-  return <Home style="onlyLogo" />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home style="onlyLogo"  />} />
+        <Route path="formulario" element={<Formulario />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable default-case */
+import React from "react";
+import "../assets/css/Home.css";
+
+const Formulario = ({ style }) => {
+    return (
+        <>
+            <p>Este es un Formulario</p>
+        </>
+    );
+};
+
+export default Formulario;
