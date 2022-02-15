020a436369d165e6ec2f224b0108c9ed93469812
Daniel Vega <danielvegac2000@mail.com>
2022-02-15 17:12:23 -0500

    Mejoras
    Mejoras significativas en el responsive y nuevas propuestas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ import "../assets/css/Footer.css"
 const Footer = ({ section }) => {
     return (
         <footer>
-            <div className="mt-5 pt-5 pb-5 footer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path
+                    fill="#00659d"
+                    fill-opacity="1"
+                    d="M0,128L80,133.3C160,139,320,149,480,170.7C640,192,800,224,960,224C1120,224,1280,192,1360,176L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+                ></path>
+            </svg>
+            <div className="  footer">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-5 col-xs-12 about-company">
--- src/components/Objetives.jsx
+++ src/components/Objetives.jsx
@@ -3,12 +3,18 @@ import "../assets/css/Objetives.css";
 
 const Objetives = ({ section }) => {
     return (
-        <section id={section.id} style={{paddingTop:"6%"}}>
+        <section
+            id={section.id}
+            class="flex-center"
+            style={{ marginTop: "9%" }}
+        >
             <div className="content-103">
                 <div className="container">
                     <div className="row">
-                        <h2 className="display-5 fw-bold text-center mb-4">Nuestros objetivos</h2>
-                        <div className="col-md-3 col-sm-3">
+                        <h2 className="font-Geomanist display-5 fw-bold text-center mb-4 mb-5">
+                            Nuestros objetivos
+                        </h2>
+                        <div className="col-lg-3 col-sm-6 col-xs-12">
                             <div className="service-post">
                                 <div className="service-content">
                                     <div className="service-icon">
@@ -26,7 +32,7 @@ const Objetives = ({ section }) => {
                                 <div className="service-hover"></div>
                             </div>
                         </div>
-                        <div className="col-md-3 col-sm-3">
+                        <div className="col-lg-3  col-sm-6 col-xs-12">
                             <div className="service-post">
                                 <div className="service-content">
                                     <div className="service-icon">
@@ -44,7 +50,7 @@ const Objetives = ({ section }) => {
                                 <div className="service-hover"></div>
                             </div>
                         </div>
-                        <div className="col-md-3 col-sm-3">
+                        <div className="col-lg-3  col-sm-6 col-xs-12">
                             <div className="service-post">
                                 <div className="service-content">
                                     <div className="service-icon">
@@ -62,7 +68,7 @@ const Objetives = ({ section }) => {
                                 <div className="service-hover"></div>
                             </div>
                         </div>
-                        <div className="col-md-3 col-sm-3">
+                        <div className="col-lg-3  col-sm-6 col-xs-12">
                             <div className="service-post">
                                 <div className="service-content">
                                     <div className="service-icon">
@@ -82,16 +88,6 @@ const Objetives = ({ section }) => {
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex-column">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path
-                        fill="#FC8890"
-                        fill-opacity="1"
-                        d="M0,160L48,170.7C96,181,192,203,288,208C384,213,480,203,576,181.3C672,160,768,128,864,133.3C960,139,1056,181,1152,181.3C1248,181,1344,139,1392,117.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-                    ></path>
-                </svg>
-              
             </div>
         </section>
     );
